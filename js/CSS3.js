ae907da1dce54b72df9650abc5f1eb83e8901c93
蒋宏伟 <fittestfish@gmail.com>
2015-03-18 11:57:41 +0800

    add css3 app

--- js/CSS3.js
+++ js/CSS3.js
@@ -1,38 +1,34 @@
-//顶部焦点图
-(function (){
-	var oUl=document.querySelector('#appbox .slider ul');
-	var aLi=oUl.children;
-	var aBtn=document.querySelectorAll('#appbox .slider ol li');
-	var now=0;
-	
-	setInterval(function (){
-		now++;
-		if(now==aLi.length)now=0;
-		tab();
-	}, 2000);
-	
-	function tab(){
-		for(var i=0;i<aBtn.length;i++){
-			aBtn[i].className='';
-		}
-		aBtn[now].className='active';
-		oUl.style.WebkitTransform='translateX(-'+now*aLi[0].offsetWidth+'px)';
-	}
-})();
+(function(){
+	// 在前面显示的元素，隐藏在后面的元素
+	var eleBack = null, eleFront = null,
+	    // 纸牌元素们 
+	    eleList = document.querySelectorAll(".list");
 
-//点击搜索
-(function (){
-	var oBtn=document.getElementById('search_btn');
-	var oDiv=document.getElementById('search_dialog');
-	var oBtnClose=oDiv.querySelector('#appbox .close');
-	var oPage=document.querySelector('#appbox .page');
-	
-	oBtn.onclick=function (){
-		oPage.style.display='none';
-		oDiv.style.display='block';
+	// 确定前面与后面元素
+	var funBackOrFront = function() {
+		for(var i = 0; i < eleList.length; i++){
+			if(hasClass(eleList[i],'out')){
+				eleBack = eleList[i];
+			}else{
+				eleFront = eleList[i];
+			}
+		}
+
 	};
-	oBtnClose.onclick=function (){
-		oPage.style.display='block';
-		oDiv.style.display='none';
-	};
+	funBackOrFront();
+
+	var oBox = document.getElementById('box');
+	oBox.addEventListener('click',function(){
+		addClass(eleFront,'out');
+		removeClass(eleFront,'in');
+		setTimeout(function(){
+			console.log(eleList);
+
+			addClass(eleBack,'in');
+			removeClass(eleBack,'out');
+			// 重新确定正反元素
+			funBackOrFront();
+		},225);
+	},false);
+
 })();
